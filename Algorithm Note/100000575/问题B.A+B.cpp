20efec3c3c260a0e65cefcfa337e4dced633df3b
Pumpkinor <pumpkinor@163.com>
2017-08-14 18:45:39 +0800

    07/08/14.02

---
+++ Algorithm Note/100000575/问题B.A+B.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	char a[15],b[15];
+	while(scanf("%s%s",a,b) != EOF){
+		for(int i = 0;i < 15;i++){
+			if(a[i] == '-'){
+				
+			}
+			if(a[i] == ','){
+				
+			}
+		}
+	}
+	
+	return 0;
+} 
