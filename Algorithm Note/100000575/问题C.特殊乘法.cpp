9157be6351ae2a0baf8faeb937338c74da00e4e8
Pumpkinor <pumpkinor@163.com>
2017-08-16 19:02:03 +0800

    07/08/16.07

--- Algorithm Note/100000575/问题C.特殊乘法.cpp
+++ Algorithm Note/100000575/问题C.特殊乘法.cpp
@@ -5,7 +5,9 @@ int main()
 	int a,b,cpb,sum = 0;
 	while(scanf("%d%d",&a,&b) != EOF){
 		while(a != 0){
+			//分别取a的每一位与b的每一位相乘，求和
 			cpb = b;
+			//对a的每移位都要与b的每一位相乘，所以b相应的要分解成一位一位数字状态多次，所以用cpb复制b的值参与操作
 			while(cpb != 0){
 				sum += (a % 10) * (cpb % 10);
 				cpb /= 10; 
