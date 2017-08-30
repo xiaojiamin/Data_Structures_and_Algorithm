e977cd0ef73f71a0501795ace99b9b5b3770c173
Pumpkinor <pumpkinor@163.com>
2017-08-30 21:13:39 +0800

    2017/8/30.04

---
+++ Basic_Data_Structures/Stack&Queue/SqQueue/SqQueue.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <malloc.h>
+#define MaxSize 5
+//存在假溢出现象
+
+typedef char ElemType;
+typedef struct
+{
+	ElemType data[MaxSize];
+	int front;
+	int rear;
+}SqQueue;
+
+void InitQueue(SqQueue *&q);
+/*初始化顺序队列*/
+void DestroyQueue(SqQueue *&q);
+/*销毁顺序队列*/
+int QueueLength(SqQueue *q);
+/*顺序队列长度*/
+bool QueueEmpty(SqQueue *q);
+/*判断顺序队列是否为空*/
+bool enQueue(SqQueue *&q,ElemType e);
+/*入队列*/
+bool deQueue(SqQueue *&q,ElemType &e);
+/*出队列*/
+
+
+void InitQueue(SqQueue *&q)
+{
+    q = (SqQueue *)malloc(sizeof(SqQueue));
+    q->front1 = q->rear = -1;
+}
+
+void DestroyQueue(SqQueue *&q)
+{
+    free(q);
+}
+
+
+int QueueLength(SqQueue *q)
+{
+
+}
+
+bool QueueEmpty(SqQueue *q)
+{
+    return(q->front == q->rear);
+}
+
+bool enQueue(SqQueue *&q,ElemType e)
+{
+    if(q->rear == MaxSize - 1)
+    //队满，上溢出
+        return false;
+    q->rear++;
+    q->data[q->rear] = e;
+    return true;
+
+}
+
+bool deQueue(SqQueue *&q,ElemType &e)
+{
+    if(q->front == q->rear)
+        return false;
+    q->front++;
+    e = q->data[q->front];
+    return true;
+}
--- Basic_Data_Structures/Stack&Queue/SqStack/SqStack.cpp
+++ Basic_Data_Structures/Stack&Queue/SqStack/SqStack.cpp
@@ -1,12 +1,69 @@
 #include<stdio.h>
+#include<malloc.h>
 
-#define MaxSize 5;
-typedef ElemType int;
+#define MaxSize 100
+typedef char ElemType;
 typedef struct
 {
     ElemType data[MaxSize];
     int top;
 }SqStack;
+
+void InitStack(SqStack *&s);
+//初始化顺序存储栈
+void DestroyStack(SqStack *&s);
+//销毁顺序栈
+bool StackEmpty(SqStack *s);
+//判断栈是否为空，空栈时返回true
+int StackLength(SqStack *s);
+//求栈的长度
+bool Push(SqStack *&s,ElemType e);
+//进栈操作
+bool Pop(SqStack *&s,ElemType &e);
+//出栈操作
+bool GetTop(SqStack *&s,ElemType &e);
+//取栈顶元素
+void DispStack(SqStack *s);
+//输出栈中的所有元素
+bool symmetry(ElemType str[]);
+//判断一个字符串是否对称
+
+int main()
+{
+    ElemType e;
+    SqStack *s;
+    ElemType st1[7] = "abccba";
+    ElemType st2[7] = {'a','b','c','a','b','c','\0'};
+    //这样初始化字符串1的时候其实是存放了{'a','b','c','c','b','a','\0'}这七个元素的
+    //st1和st2的写法等价
+    printf("1.初始化栈s\n");
+    InitStack(s);
+    printf("2.栈s为%s\n",(StackEmpty(s)?"空":"非空"));
+    printf("3.依次进栈元素a,b,c,d,e,f\n");
+    Push(s,'a');
+    Push(s,'b');
+    Push(s,'c');
+    Push(s,'d');
+    Push(s,'e');
+    Push(s,'f');
+    printf("4.栈s为%s\n",(StackEmpty(s)?"空":"非空"));
+    printf("5.栈s的长度为%d\n",StackLength(s));
+    printf("6.从栈顶到栈底依次打印输出栈s:\n");
+    DispStack(s);
+    GetTop(s,e);
+    printf("7.栈顶元素为:%c\n",e);
+    printf("8.依次出栈序列:\n");
+    while(!StackEmpty(s)){
+        Pop(s,e);
+        printf("%c出栈\n",e);
+    }
+    printf("9.栈s为%s\n",(StackEmpty(s)?"空":"非空"));
+    printf("10.销毁栈\n");
+    printf("11.字符串1“abccba”,%s\n",(symmetry(st1)?"对称":"不对称"));
+    printf("12.字符串2“abcabc”,%s\n",(symmetry(st2)?"对称":"不对称"));
+    return 0;
+}
+
 
 void InitStack(SqStack *&s)
 {
@@ -22,6 +79,11 @@ void DestroyStack(SqStack *&s)
 bool StackEmpty(SqStack *s)
 {
     return(s->top == -1);    //空栈时栈顶指针top为-1
+}
+
+int StackLength(SqStack *s)
+{
+        return(s->top + 1);
 }
 
 bool Push(SqStack *&s,ElemType e)
@@ -40,10 +102,30 @@ bool Pop(SqStack *&s,ElemType &e)
         return false;
     }
     e = s->data[s->top];
+    s->top--;
+    return true;
+
+}
+
+bool GetTop(SqStack *&s,ElemType &e)
+{
+    if(s->top == -1){   //判断空栈情况
+        return false;
+    }
+    e = s->data[s->top];
     return true;
 }
 
-bool symmetry(ElemType str[]){
+void DispStack(SqStack *s)
+{
+    for(int i = s->top;i >= 0;i--){
+        printf("%c ",s->data[i]);
+    }
+    printf("\n");
+}
+
+bool symmetry(ElemType str[])
+{
     int i;
     ElemType e;
     SqStack *st;
